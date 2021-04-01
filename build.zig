44241306714a8eb425a84c2258c89865f24cecbd
Asherah Connor <ashe@kivikakk.ee>
2021-04-01 11:52:04 +1100

    build: add requestz package

--- build.zig
+++ build.zig
@@ -1,5 +1,30 @@
 const std = @import("std");
-const packages = @import("vendor/packages.zig");
+
+// Ideally we'd use vendor/requestz/lib/packages.zig directly, but not sure
+// how best to deal with relative paths (since it tries to resolve them from
+// our root).
+
+pub const network = std.build.Pkg{
+    .name = "network",
+    .path = "vendor/requestz/lib/zig-network/network.zig",
+    .dependencies = null,
+};
+
+pub const http = std.build.Pkg{
+    .name = "http",
+    .path = "vendor/requestz/lib/h11/lib/http/src/main.zig",
+    .dependencies = null,
+};
+
+var h11_dependencies = [_]std.build.Pkg{
+    http,
+};
+
+pub const h11 = std.build.Pkg{
+    .name = "h11",
+    .path = "vendor/requestz/lib/h11/src/main.zig",
+    .dependencies = &h11_dependencies,
+};
 
 pub fn build(b: *std.build.Builder) void {
     // Standard target options allows the person running `zig build` to choose
@@ -15,9 +40,10 @@ pub fn build(b: *std.build.Builder) void {
     const exe = b.addExecutable("requestz-trying", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
-    // exe.addPackagePath("requestz", "vendor/requestz/src/main.zig");
-    exe.addPackage(packages.requestz);
-    // exe.addPackagePath("network", "vendor/requestz/lib/zig-network/network.zig");
+    exe.addPackage(.{ .name = "requestz", .path = "vendor/requestz/src/main.zig", .dependencies = &.{
+        network,
+        h11,
+    } });
     exe.install();
 
     const run_cmd = exe.run();
